7706285b9b58d5addc322a430468a2867d4387c6
Taylor Lokombe <taylor@dwebserver.net>
2025-09-17 21:22:53 +0200

    Find away to build the logo assest for Cpanel upload

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -1,10 +1,13 @@
+import logoImage from '../../images/cof_logo_three.png';
+
 export default function ApplicationLogo(props)
 {
     const { className, ...rest } = props;
     return (
         <img
             {...rest}
-            src="/storage/image/cof_logo_three.png"
+            //src="/images/cof_logo_three.png"
+            src={logoImage}
             alt="Community of Faith Logo"
             className={className}
         />
